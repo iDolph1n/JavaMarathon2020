80057b8acdb79ed14aa4965887ac16f4cd6f3c37
vital <ny.86reg@mail.ru>
2025-01-31 19:47:03 +0500

    Add tasks Day3 comleted

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+
+            if (y == 0)
+                break;
+            System.out.println(x / y);
+        }
+        sc.close();
     }
 }
