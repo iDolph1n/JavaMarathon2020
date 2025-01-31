9a6e05870ad025b2d146021f6a73d5267f1497e8
vital <ny.86reg@mail.ru>
2025-01-31 12:31:01 +0500

    Add task Day1 completed

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 8;
+        for (int i = 1; i <= 10; i++) {
+            int c = i * k;
+            System.out.println(i + " X " + k + " = " + c);
+        }
     }
 }
